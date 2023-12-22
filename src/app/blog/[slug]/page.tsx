47362764ef431e0fed3022f74f65a09651834469
Ananth Narayan <narayanananth26@gmail.com>
2023-12-22 11:00:09 +0530

    chore:create /blog/[slug] page

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import { getBlog } from "../../../lib/graphql/functions";
+import Navbar from "../../../components/Navbar/Navbar";
+import BlogContent from "../../../components/BlogContent/page";
+
+const Blog = async ({ params }) => {
+	const blog = await getBlog(params.slug);
+	return (
+		<>
+			<Navbar />
+			<BlogContent blog={blog} />
+		</>
+	);
+};
+
+export default Blog;
