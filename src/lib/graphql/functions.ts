d6fe4f974fe9feeda3c24c8c5fa0320cb4ba121c
Ananth Narayan <narayanananth26@gmail.com>
2023-12-22 11:02:03 +0530

    chore:implement data fetching from contentful using graphql

---
+++ src/lib/graphql/functions.ts
@@ -0,0 +1,60 @@
+// functions.ts
+
+import { GET_BLOGS_QUERY } from "./queries";
+
+export async function getBlogs() {
+	const res = await fetch(
+		`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+			},
+			body: JSON.stringify({ query: GET_BLOGS_QUERY }),
+		}
+	);
+
+	const { data } = await res.json();
+
+	return data.blogCollection.items;
+}
+
+export async function getBlog(slug: string) {
+	const res = await fetch(
+		`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+			},
+			body: JSON.stringify({
+				query: `
+        {
+          blogCollection(where: { slug: "${slug}" }) {
+            items {
+              headImg {
+                url
+              }
+              authorName
+              authorImage {
+                url
+              }
+              title
+              date
+              blogContent {
+                json
+              }
+            }
+          }
+        }
+      `,
+			}),
+		}
+	);
+
+	const { data } = await res.json();
+
+	return data.blogCollection.items[0];
+}
