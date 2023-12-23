6cbec42e6548b2ff568aca614603a6e1e30ef42b
Ananth Narayan <narayanananth26@gmail.com>
2023-12-23 19:15:38 +0530

    refactor:add Navbar and Footer components to layout

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,15 +1,9 @@
 import { getBlog } from "../../../lib/graphql/functions";
-import Navbar from "../../../components/Navbar/Navbar";
 import BlogContent from "../../../components/Blog/BlogContent/page";
 
 const Blog = async ({ params }) => {
 	const blog = await getBlog(params.slug);
-	return (
-		<>
-			<Navbar />
-			<BlogContent blog={blog} />
-		</>
-	);
+	return <BlogContent blog={blog} />;
 };
 
 export default Blog;
