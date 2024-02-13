3ecc96dece753e0bd0e505fdf193b259b83d59d4
Ananth Narayan <narayanananth26@gmail.com>
2024-02-13 22:05:03 +0530

    feat: add dynamic metadata generation in blog page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,28 +1,25 @@
-import { getBlog } from "../../../lib/graphql/functions";
+import { getBlog } from "../../../lib/graphql/fetchers";
 import BlogContent from "../../../components/Blog/BlogContent/page";
 import type { Metadata } from "next";
-export const metadata:Metadata = {
-	title: 'The Art of Cold Messaging',
-	description: 'Cold messaging is a lead generation strategy that helps to connect with strangers and pitch services or products, while helping you unlock new opportunities! Follow these 7 strategies to optimize your cold messaging and increase your response rate by a 100%.',
-	keywords: 'Cold Messaging, Cold Messaging on LinkedIn, Cold Messaging Automation, LinkedIn Outreach, LinkedIn Messaging, LinkedIn Automation',
-	robots: 'index, follow', 
-	authors:[
-		{
-			"name":"Hisham Hashir",
-			"url":"https://www.coldmessager.com/blog/the-art-of-cold-messaging"
-		}
-	],
-	
 
-	
-  };
-  
+export async function generateMetadata({
+	params,
+}: {
+	params: { slug: string };
+}): Promise<Metadata> {
+	const { metaTags } = await getBlog(params.slug);
+	return {
+		title: metaTags?.metaTitle || "",
+		description: metaTags?.metaDescription || "",
+		keywords: metaTags?.metaKeywords || "",
+		robots: metaTags?.metaRobots || "",
+		authors: metaTags?.metaAuthor || [],
+	};
+}
+
 const Blog = async ({ params }) => {
 	const blog = await getBlog(params.slug);
-	return<BlogContent blog={blog} />
-
-
-	
+	return <BlogContent blog={blog} />;
 };
 
 export default Blog;
